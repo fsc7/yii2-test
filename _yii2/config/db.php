043f400d1d9d721ea35c659ac28023b0f4b6974e
Felipe Costa <fsc7@yahoo.com>
2017-12-12 09:24:06 +0000

    Git Ignore

--- _yii2/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=sql8.main-hosting.eu;dbname=u112213902_serv',
-    'username' => 'u112213902_serv',
-    'password' => '#Conversa1',
-    'charset' => 'utf8',
-];
